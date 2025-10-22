a44ee2defa8b6b0ae375e80491812d2bd683e650
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2025-10-22 13:41:58 -0300

    product com cache pronto

--- src/main/java/store/product/ProductService.java
+++ src/main/java/store/product/ProductService.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.cache.annotation.Cacheable;
 
 @Service
 public class ProductService {
@@ -14,6 +16,7 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @CacheEvict(cacheNames = { "products-list" }, key = "'all'")
     public Product create(Product product) {
         if (null == product.name()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
@@ -35,6 +38,7 @@ public class ProductService {
         ).to();
     }
 
+    @Cacheable(cacheNames = "products-list", key = "'all'")
     public List<Product> findAll() {
         return StreamSupport.stream(
             productRepository.findAll().spliterator(), false)
@@ -42,6 +46,7 @@ public class ProductService {
             .toList();
     }    
 
+    @Cacheable(cacheNames = "product-by-id", key = "#id")
     public Product findById(String id) {
         return productRepository.findById(id)
             .map(ProductModel::to)
@@ -50,6 +55,7 @@ public class ProductService {
             ));
     }
 
+    @CacheEvict(cacheNames = { "product-by-id" }, key = "#id")
     public void delete(String id) {
         if (!productRepository.existsById(id)) {
             throw new ResponseStatusException(
