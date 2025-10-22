c21b5fc38e0a4f79e64042e6747abc0a2b9f5630
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2025-10-22 17:00:34 -0300

    melhorando cache

--- src/main/java/store/product/ProductService.java
+++ src/main/java/store/product/ProductService.java
@@ -5,10 +5,12 @@ import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 
 @Service
 public class ProductService {
@@ -16,7 +18,14 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    @CacheEvict(cacheNames = { "products-list" }, key = "'all'")
+    @Caching(
+        evict = {
+            @CacheEvict(cacheNames = "products-list", allEntries = true)
+        },
+        put = {
+            @CachePut(cacheNames = "product-by-id", key = "#result.id", unless = "#result == null")
+        }
+    )
     public Product create(Product product) {
         if (null == product.name()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
@@ -38,7 +47,7 @@ public class ProductService {
         ).to();
     }
 
-    @Cacheable(cacheNames = "products-list", key = "'all'")
+    @Cacheable(cacheNames = "products-list", unless = "#result == null || #result.isEmpty()")
     public List<Product> findAll() {
         return StreamSupport.stream(
             productRepository.findAll().spliterator(), false)
@@ -46,7 +55,7 @@ public class ProductService {
             .toList();
     }    
 
-    @Cacheable(cacheNames = "product-by-id", key = "#id")
+    @Cacheable(cacheNames = "product-by-id", key = "#id", unless = "#result == null")
     public Product findById(String id) {
         return productRepository.findById(id)
             .map(ProductModel::to)
@@ -55,7 +64,12 @@ public class ProductService {
             ));
     }
 
-    @CacheEvict(cacheNames = { "product-by-id" }, key = "#id")
+    @Caching(
+        evict = {
+            @CacheEvict(cacheNames = "product-by-id", key = "#id"),
+            @CacheEvict(cacheNames = "products-list", allEntries = true)
+        }
+    )
     public void delete(String id) {
         if (!productRepository.existsById(id)) {
             throw new ResponseStatusException(
